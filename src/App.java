75ddcaf44892db9e00c05ebef87d88ca316beef3
Admin <mohammadasaadgo@gmail.com>
2026-02-07 08:26:58 +0200

    update: add handling error hole application.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Application.start();
+        try{
+            Application.start();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
